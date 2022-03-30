251899f90cab3c9e6c512ebc678b5b124ca28f3f
sikako <jack71805@gmail.com>
2022-03-30 00:28:06 -0700

    function getIndex()

--- 5hw0800224.c
+++ 5hw0800224.c
@@ -4,7 +4,6 @@
 #include <regex.h>
 #include <string.h>
 #include "function.h"
-#define toFind "/=\\d{1,}=/"
 
 
 /*------------------------------------------------------
@@ -13,17 +12,51 @@ Subscrition:Merge two files and write it in a new file |
 Author:Sikako                                          |
 -------------------------------------------------------*/
 
+// Function to print the result
+void print_result(int value)
+{
+  
+    // If pattern found
+    if (value == 0) {
+        printf("Pattern found.\n");
+    }
+  
+    // If pattern not found
+    else if (value == REG_NOMATCH) {
+        printf("Pattern not found.\n");
+    }
+  
+    // If error occured during Pattern
+    // matching
+    else {
+        printf("An error occured.\n");
+    }
+}
 
+
+// Function to get the index from =#=
+int getIndex(FILE *fp){
+  int index;
+  char s[4];
+  fgetc(fp);					// get rid of =
+  fscanf(fp, "%[^=]s", s);
+  index = atoi(s);
+  fgetc(fp);					// get rid of =
+  return index;
+}
+
+// main function
 int main(){
  char filename1[20] = "ex33_1.txt";
  char filename2[20] = "ex33_2.txt";
  char filename3[20] = "ans33.txt";
  FILE *fp1, *fp2, *fp3;
  char stringData[601];                          // buffer
- int index_1, index_2;				// store the =#= of each file
- char cFrom1, cFrom2;
+ int index_1, index_2 = 0;				// store the =#= of each file. -1 when 
+ char cFrom1, cFrom2 = ' ';
  int printFrom1 = 0;				// bool to check which file to print
- int end1, end2 = 0				// bool to check if file is end
+ int end1, end2 = 0;				// bool to check if file is end
+ regex_t regex;
 
 
  // open the files
@@ -34,15 +67,16 @@ int main(){
 // fscanf(fp1, "%s", stringData);
 // fprintf(fp3, "%s", stringData);
 
-while (1) {
-  cFrom1 = fgetc(fp1);
-
-  if(c== '=') putchar(c);   
-} 
 
 
+ index_1 = getIndex(fp1);
+ index_2 = getIndex(fp2);
 
-
+ if(index_1 < index_2){
+   printFrom1 = 1;
+   fscanf(fp1, "%[^=]s", stringData);		// get line between =#=
+   printf("%s", stringData);
+ }
 
  // close the files
  closeFile(fp1, filename1);
